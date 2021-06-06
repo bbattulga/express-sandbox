b4f53c67cc42e0477336c2bff4a69f1ec7cb420a
battulga <battulga.dev@gmail.com>
2021-06-07 02:15:08 +0800

    fix deployment

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Senpaii' });
+  res.render('index', { title: 'Senpaii3' });
 });
 
 module.exports = router;
