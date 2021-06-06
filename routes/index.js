7427b87ca39dd206eb26868dae6cfa4371b74956
battulga <battulga.dev@gmail.com>
2021-06-07 01:56:13 +0800

    change title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Title updated' });
+  res.render('index', { title: 'Title' });
 });
 
 module.exports = router;
