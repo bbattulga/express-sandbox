bd84204980384055f6cd8386a1b7d36da97ad21c
battulga <battulga.dev@gmail.com>
2021-06-07 01:24:39 +0800

    title change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express Sandbox Minor Change' });
+  res.render('index', { title: 'Express Sandbox' });
 });
 
 module.exports = router;
